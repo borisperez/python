6d786fdf6c1afee87d5485ba55124eb5eb6d68db
BORIS VITORIO PEREZ <boris.bperez@gmail.com>
2018-10-30 16:30:19 -0400

    Update checkports.py

---
+++ checkports.py
@@ -0,0 +1,17 @@
+import socket
+import sys
+
+host = sys.argv[1]
+portList = sys.argv[2].split(",")
+
+print "Check open ports on Host: " + host
+while portList:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(2)
+    port = int(portList.pop())
+    result = sock.connect_ex((host,port))
+    if result == 0:
+       print "Port: " + str(port) + " is Open"
+    else:
+       print "Port: " + str(port) + " is Close or listening services is down!"
+sock.close()
