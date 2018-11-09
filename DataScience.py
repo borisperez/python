4abc79a3289f0bcff8e322980b6b3ff6888a0e73
angelapaiva <44452094+angelapaiva@users.noreply.github.com>
2018-11-08 22:08:33 -0500

    Update DataScience.py

--- DataScience.py
+++ DataScience.py
@@ -1,1 +1,17 @@
-teste
+df_titanic["Class"] = ''
+
+def classDescription(x):
+    if x  == 1:
+        return  'Upper'
+    elif x  == 2:
+        return 'Middle'      
+    elif x  == 3:
+        return  'Lower'
+    else:
+        return 'Not Identified'
+
+df_titanic['Class'] = df_titanic.apply(lambda row: classDescription(row['Pclass']), axis=1)
+
+
+df_titanic.head()
+    
